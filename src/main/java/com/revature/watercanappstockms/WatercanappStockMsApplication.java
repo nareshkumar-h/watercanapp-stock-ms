df4128d96c7529245257d373d57d5f412b9a8744
SHARMILAG97 <srdssharmig97@gmail.com>
2019-10-24 11:25:46 +0530

    First Commit

---
+++ src/main/java/com/revature/watercanappstockms/WatercanappStockMsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.watercanappstockms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatercanappStockMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatercanappStockMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/watercanappstockms/WatercanappStockMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.watercanappstockms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatercanappStockMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
